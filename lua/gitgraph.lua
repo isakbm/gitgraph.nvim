f5131a94a7867544319741e42a5266f08766171f
Robert Günzler <r@gnzler.io>
2024-08-04 13:05:37 +0200

    Use plain 'gitgraph' filtype
    Signed-off-by: Robert Günzler <r@gnzler.io>

--- lua/gitgraph.lua
+++ lua/gitgraph.lua
@@ -2008,7 +2008,7 @@ function M.draw(options, args)
   do
     if not M.buf or not vim.api.nvim_buf_is_valid(M.buf) then
       M.buf = vim.api.nvim_create_buf(false, true)
-      vim.api.nvim_set_option_value('filetype', 'gitgraph.nvim', { buf = M.buf })
+      vim.api.nvim_set_option_value('filetype', 'gitgraph', { buf = M.buf })
     end
   end
 
